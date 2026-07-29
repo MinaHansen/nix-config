ce226eeb79cf7fffcb3a0bbdb50ab9faf14f9aa8
Mina <github.rarity719@passinbox.com>
2026-07-29 22:44:18 +0200

    Fixed hash

--- flake/modules/fonts.nix
+++ flake/modules/fonts.nix
@@ -7,6 +7,7 @@ let
     src = pkgs.fetchzip {
       url    = "https://github.com/googlefonts/morisawa-biz-ud-mincho"
              + "/releases/download/v1.06/morisawa-biz-ud-mincho-fonts.zip";
+      hash   = "sha256-TuNYguBCHkln8jbker/HxTNZS8cI1vJDRrT1PGmNSqE=";
     };
     nativeBuildInputs = [ pkgs.installFonts ];
     sourceRoot        = "morisawa-biz-ud-mincho-fonts/fonts/ttf";
