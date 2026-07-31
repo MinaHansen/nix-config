5e47a76e063e565b99898684d5878fb6bd41ac68
Mina <github.rarity719@passinbox.com>
2026-07-31 15:38:43 +0200

    Commented default

--- hosts/default.nix
+++ hosts/default.nix
@@ -12,28 +12,28 @@
     ../modules/user.nix
   ];
 
-  my.kernel = {
-    variant       = "bore";
-    cpusched      = "bore";
-    processorOpt  = "x86_64-v3";
-    lto           = "full";
-    hzTicks       = "1000";
-    bbr3          = true;
-    hardened      = false;
-    fixGpp0Wakeup = true;
-  };
+  #my.kernel = {
+  #  variant       = "bore";
+  #  cpusched      = "bore";
+  #  processorOpt  = "x86_64-v3";
+  #  lto           = "full";
+  #  hzTicks       = "1000";
+  #  bbr3          = true;
+  #  hardened      = false;
+  #  fixGpp0Wakeup = true;
+  #};
 
-  my.features = {
-    steam    = true;
-    discord  = true;
-    devTools = true;
-    spotify  = true;
-  };
+  #my.features = {
+  #  steam    = true;
+  #  discord  = true;
+  #  devTools = true;
+  #  spotify  = true;
+  #};
 
-  my.input = {
-    keyboardLayoutShort = "jp";
-    keyboardLayout      = "jp106";
-  };
+  #my.input = {
+  #  keyboardLayoutShort = "jp";
+  #  keyboardLayout      = "jp106";
+  #};
 
   #networking.hostName = "minanixos";
   #time.timeZone       = "Europe/Oslo";
