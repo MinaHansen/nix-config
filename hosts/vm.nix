e61c43ca8df3818189e0244facd61564f96f75b6
Mina <github.rarity719@passinbox.com>
2026-07-31 15:36:19 +0200

    Changed lto setting

--- hosts/vm.nix
+++ hosts/vm.nix
@@ -8,7 +8,7 @@
     variant       = "bore";
     cpusched      = "bore";
     processorOpt  = "x86_64-v3";
-    lto           = "none";
+    lto           = "slim";
     hzTicks       = "1000";
     bbr3          = true;
     hardened      = false;
