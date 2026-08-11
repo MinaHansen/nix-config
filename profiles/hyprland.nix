11f3b7cdc06f4161e46fb5b5071101ba091b8b4f
Mina <github.rarity719@passinbox.com>
2026-08-11 10:01:08 +0200

    Import sddm.nix

--- profiles/hyprland.nix
+++ profiles/hyprland.nix
@@ -1,5 +1,9 @@
 { inputs, pkgs, ... }:
 {
+  imports = [
+    ../modules/sddm.nix
+  ];
+
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
--- profiles/plasma.nix
+++ profiles/plasma.nix
@@ -1,4 +1,7 @@
 { ... }:
 {
+  imports = [
+    ../modules/sddm.nix
+  ];
   services.desktopManager.plasma6.enable = true;
 }
