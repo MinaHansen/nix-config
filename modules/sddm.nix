8ec6adc0112604f9a203a43a44b1853100794fe6
Mina <github.rarity719@passinbox.com>
2026-08-11 09:54:57 +0200

    Added option

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,10 +1,17 @@
-{ inputs, config, ... }:
+{ inputs, config, lib, ... }:
+let
+  cfg = config.my.desktop;
+in
 {
+  options.my.desktop = {
+    type = lib.mkOption { type = lib.types.str; };
+  };
+
   imports = [inputs.silentSDDM.nixosModules.default];
   programs.silentSDDM = {
     enable = true;
     theme = "default";
   };
 
-  services.displayManager.defaultSession = config.my.desktop.type;
+  services.displayManager.defaultSession = cfg.type;
 }
